484dbdffabd832eeeefccf266b6a769faf327a9f
khairul alam <khairul.alam@vivacomsolutions.com>
2023-01-31 01:05:56 +0600

    initial

---
+++ 2.PatternSlidingWindow/UnitTest.cs
@@ -0,0 +1,32 @@
+namespace PatternSlidingWindow
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Arrange
+            double expected = 5;
+
+            // Act
+            double actual = 5;
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+
+        [Fact]
+        public void GetMaximumSumSubarrayOfSizeKTest()
+        {
+            // Arrange
+
+            // Act
+
+            //Assert
+
+        }
+
+
+    }
+}
