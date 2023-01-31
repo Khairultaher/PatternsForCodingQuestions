6348d081fd227119d97dbac66150b2789b460fe1
khairul alam <khairul.alam@vivacomsolutions.com>
2023-01-31 17:25:11 +0600

    xUnit Moq Shouldly added

---
+++ 2.PatternSlidingWindow/2_MaximumSumSubarrayOfSizeKTest.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace PatternSlidingWindow
+{
+    public class MaximumSumSubarrayOfSizeKTest
+    {
+        [Theory]
+        [InlineData(new int[] { 2,7,5,4}, 9, new int[] { 0,1})]
+        public void TwoSumTest(int[] nums, int sum, int[] expected)
+        {
+            // Arrange
+            Dictionary<int, int> keyValues= new Dictionary<int, int>();
+            int[] actual = new int[2];
+
+            // Act
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int dif = sum - nums[i];
+
+                if (keyValues.ContainsKey(dif))
+                {
+                    actual = new int[] { keyValues[dif], i };
+                    break;
+                }
+                else 
+                {
+                    keyValues.Add(nums[i], i);
+                }
+            }
+            //Assert
+            expected.ShouldBe(actual);
+        }
+
+
+    }
+}
--- 2.PatternSlidingWindow/3_SmallestSubarrayWithGivenSumTest.cs
+++ 2.PatternSlidingWindow/3_SmallestSubarrayWithGivenSumTest.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace PatternSlidingWindow
 {
-    public class UnitTest
+    public class SmallestSubarrayWithGivenSumTest
     {
 
         [Theory]
